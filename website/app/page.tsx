1f89cf0eda85f499ed401fcd2cdcefa50570870b
Sristi Gupta <sristigupta788@gmail.com>
2026-09-09 13:38:18 +0530

    addD

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -1,25 +1,33 @@
 "use client";
 
-import { useState } from "react";
-import Landing from "./src/components/landing/Landing";
-import About from "./About";
+import {useEffect ,useState} from "react";
 
-export default function App() {
-  const [currentSection, setCurrentSection] = useState("landing");
+export default function Home(){
+  const [load , setload] = useState(false);
+  const [enter , setenter] = useState(false);
 
-  return (
-    <div>
-      {currentSection === "landing" && (
-        <Landing
-          onOpenAbout={() => setCurrentSection("about")}
-        />
-      )}
+  useEffect(()=>{
+    setTimeout(()=>{
+      setload(true);
+    }, 1000)
+  },[])
+  const handle =() =>{
+    setenter(true);
 
-      {currentSection === "about" && (
-        <About
-          onClick={() => setCurrentSection("landing")}
-        />
-      )}
-    </div>
-  );
+  }
+  setTimeout(()=>{
+    setenter(false);
+  }, 1000)
+  return(
+    <main className = "relative h-scrren min-h-[650px] w-screen overflow-hidden bg-[#08070a] text-[#4f4f4f]"> 
+    
+    {/* background */}
+
+
+
+    <div className="pointer-events-none absolute insert-0 overflow-hidden bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-[#08070a] via-[#08070a] to-[#08070a]">
+      <div className="abosolute -right-32 -top-44 h-[420px] w-[420px] animate-pulse rounded-full bg-[#9b5f70]/25 blur-[100px]">
+      </div>
+    </main>
+  )
 }
