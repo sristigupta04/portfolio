98c17a635603e695a7cd7724511131e3185dabb3
Sristi Gupta <sristigupta788@gmail.com>
2026-09-09 23:10:54 +0530

    add

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -1,8 +1,8 @@
 import Hero from "./components/landing/Hero";
 import Project from "./components/projects/section";
-import About from "./components/about/about";
-import Experience from "./components/experience/experience";
-import Contact from "./components/contact/contact";
+import About from "./components/about/About";
+import Experience from "./components/experience/Experience";
+import Contact from "./components/contact/Contact";
 
 export default function Home(){
   return(
